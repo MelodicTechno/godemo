2cc5f994bf7577341001efb32d4045eb3b1bff6e
MelodicTechno <617179025@qq.com>
2025-06-26 11:02:27 +0800

    'init'

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"exchangeapp/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.InitConfig()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":3000")
+}
